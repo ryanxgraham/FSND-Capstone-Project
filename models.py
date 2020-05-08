608575dccf84eb292d33bd3310e26c7f7f7ad873
ryanxgraham <ryanxgraham@gmail.com>
2020-05-08 07:15:14 -0500

    added files

--- models.py
+++ models.py
@@ -1,1 +1,26 @@
+import os
+from sqlalchemy import Column, String, create_engine
+from flask_sqlalchemy import SQLAlchemy
+import json
+database_name = 'capstone'
+user_name = "postgres"
+password = "postgres"
+database_path = "postgres://{}:{}@{}/{}".format(
+  user_name,
+  password,
+  'localhost:5432',
+  database_name)
+# database_path = os.environ['DATABASE_URL']
 
+db = SQLAlchemy()
+
+'''
+setup_db(app)
+    binds a flask application and a SQLAlchemy service
+'''
+def setup_db(app, database_path=database_path):
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.app = app
+    db.init_app(app)
+    db.create_all()
--- setup.sh
+++ setup.sh
@@ -1,1 +1,5 @@
-
+#!/bin/sh
+export AUTH0_DOMAIN=rgraham.auth0.com
+export ALGORITHMS=['RS256']
+export API_AUDIENCE=capstone
+export CLIENT_ID=TMWB0ZYKbwdiX7DR9ogU4VPzdkXaxGG2
